f5612fe739f9ab2c027f71e816ef851e9583d07c
Jace Walker <jacewalker@me.com>
2022-04-18 16:16:05 +1000

    Reduction of code and implementation of additional threading

--- port-check.rb
+++ port-check.rb
@@ -1,16 +1,17 @@
 require 'socket'
 require 'timeout'
+require 'net/ping'
 
 # Obtains information from user for target provided as command-line arguments 1 & 2
 # PORT = ARGV[0] || 22
 HOST = ARGV[0] || 'localhost'
 
-def scan250
-    for p in 1...250 do
+def scan(startPort, endPort)
+    until startPort == endPort do
         # Attempts TCP connection and will timeout after 1 second
         begin
-            results = Timeout.timeout(0.009) do
-                socket = TCPSocket.new(HOST, p)
+            results = Timeout.timeout(0.008) do
+                socket = TCPSocket.new(HOST, startPort)
                 $status = "open"
             end
         # Error handling for closed ports
@@ -21,84 +22,39 @@ def scan250
     
         # Outputs result
         if $status == "open"
-            puts "Port #{p} is #{$status}"
+            puts "Port #{startPort} is #{$status}"
         end
+
+        startPort += 1
     end
 end
 
+def main
+    startTime = Time.now
 
-def scan500
-    for p in 251...500 do
-        # Attempts TCP connection and will timeout after 1 second
-        begin
-            results = Timeout.timeout(0.009) do
-                socket = TCPSocket.new(HOST, p)
-                $status = "open"
-            end
-        # Error handling for closed ports
-        rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT, Timeout::Error
-            $status = "closed"
-        end
-    
-    
-        # Outputs result
-        if $status == "open"
-            puts "Port #{p} is #{$status}"
-        end
-    end
+    t1 = Thread.new{scan(1, 100)}
+    t2 = Thread.new{scan(101, 200)}
+    t3 = Thread.new{scan(201, 300)}
+    t4 = Thread.new{scan(301, 400)}
+    t5 = Thread.new{scan(401, 500)}
+    t6 = Thread.new{scan(501, 600)}
+    t7 = Thread.new{scan(601, 700)}
+    t8 = Thread.new{scan(701, 800)}
+    t9 = Thread.new{scan(801, 900)}
+    t10 = Thread.new{scan(901, 1000)}
+
+    t1.join
+    t2.join
+    t3.join
+    t4.join
+    t5.join
+    t6.join
+    t7.join
+    t8.join
+    t9.join
+    t10.join
+
+    puts "Duration: #{Time.now - startTime}s"
 end
 
-def scan750
-    for p in 501...750 do
-        # Attempts TCP connection and will timeout after 1 second
-        begin
-            results = Timeout.timeout(0.009) do
-                socket = TCPSocket.new(HOST, p)
-                $status = "open"
-            end
-        # Error handling for closed ports
-        rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT, Timeout::Error
-            $status = "closed"
-        end
-    
-    
-        # Outputs result
-        if $status == "open"
-            puts "Port #{p} is #{$status}"
-        end
-    end
-end
-
-def scan1000
-    for p in 751...1000 do
-        # Attempts TCP connection and will timeout after 1 second
-        begin
-            results = Timeout.timeout(0.009) do
-                socket = TCPSocket.new(HOST, p)
-                $status = "open"
-            end
-        # Error handling for closed ports
-        rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT, Timeout::Error
-            $status = "closed"
-        end
-    
-    
-        # Outputs result
-        if $status == "open"
-            puts "Port #{p} is #{$status}"
-        end
-    end
-end
-
-puts "Started at #{Time.now}"
-t1 = Thread.new{scan250()}
-t2 = Thread.new{scan500()}
-t3 = Thread.new{scan750()}
-t4 = Thread.new{scan1000()}
-
-t1.join
-t2.join
-t3.join
-t4.join
-
-puts "Ended at #{Time.now}"
+main
